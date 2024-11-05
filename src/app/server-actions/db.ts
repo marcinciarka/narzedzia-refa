31a4618cf1e8b8773581e4da6bcb6aed7d7314a3
Marcin Ciarka <marcin@oazoapps.com>
2024-11-05 10:12:47 +0100

    Update database connection to use NARZEDZIA_REFA__URL_NON_POOLING environment variable

--- src/app/server-actions/db.ts
+++ src/app/server-actions/db.ts
@@ -3,11 +3,11 @@ import postgres from "postgres";
 import { PostgresJSDialect } from "kysely-postgres-js";
 import { DB } from "@/types/db";
 
-if (!process.env.DATABASE_URL_NON_POOLING) {
-  throw new Error("DATABASE_URL_NON_POOLING is not set");
+if (!process.env.NARZEDZIA_REFA__URL_NON_POOLING) {
+  throw new Error("NARZEDZIA_REFA__URL_NON_POOLING is not set");
 }
 
-const pg = postgres(process.env.DATABASE_URL_NON_POOLING);
+const pg = postgres(process.env.NARZEDZIA_REFA__URL_NON_POOLING);
 export const db = new Kysely<DB>({
   dialect: new PostgresJSDialect({
     postgres: pg,
